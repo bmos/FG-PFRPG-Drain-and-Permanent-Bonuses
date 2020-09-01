0696df6ba147d29c863f542844ec11f7e789ff19
Wil Thieme <wil.thieme@protonmail.com>
2020-09-01 10:28:27 -0400

    begin temp effects stack-avoidance

--- campaign/scripts/abil_totalperms.lua
+++ campaign/scripts/abil_totalperms.lua
@@ -7,21 +7,43 @@ function onInit()
 	DB.addHandler('charsheet.*.abilities.*.abilperms.*.bonus_type', 'onUpdate', onPermUpdate)
 	DB.addHandler('charsheet.*.abilities.*.abilperms', 'onChildDeleted', onPermUpdate)
 
+	-- Include ability and character names in window title
 	local sCharName = DB.getValue(getDatabaseNode().getChild('...'), 'name', '')
 	local sAbility = getDatabaseNode().getName()
 	title.setValue(StringManager.titleCase(sAbility) .. ' - ' .. sCharName)
 end
 
+function onClose()
+	DB.removeHandler('charsheet.*.abilities.*.abilperms.*.permnum', 'onUpdate', onPermUpdate)
+	DB.removeHandler('charsheet.*.abilities.*.abilperms.*.bonus_type', 'onUpdate', onPermUpdate)
+	DB.removeHandler('charsheet.*.abilities.*.abilperms', 'onChildDeleted', onPermUpdate)
+end
+
+
 local function addPerms(nodeAbil)
+	local rActor = ActorManager.getActor('pc', nodeAbil.getChild('...'))
+	
+	local sAbil = ''
+	if nodeAbil.getName() == 'strength' then sAbil = 'STR' end
+	if nodeAbil.getName() == 'dexterity' then sAbil = 'DEX' end
+	if nodeAbil.getName() == 'constitution' then sAbil = 'CON' end
+	if nodeAbil.getName() == 'intelligence' then sAbil = 'INT' end
+	if nodeAbil.getName() == 'wisdom' then sAbil = 'WIS' end
+	if nodeAbil.getName() == 'charisma' then sAbil = 'CHA' end
+	
 	local aBonuses = {}
 	for _,vNode in pairs(DB.getChildren(nodeAbil, 'abilperms')) do
 		local nPerm = DB.getValue(vNode, 'permnum', 0)
 		local sType = DB.getValue(vNode, 'bonus_type', 0)
 
 		if nPerm > 0 then
+			-- Don't stack bonuses of the same type
+			local aAbilEffects, aSubTotals = EffectManagerDaPB.getEffectsByType(rActor, sAbil, sType, aFilter, rFilterActor, bTargetedOnly)
+			
 			if aBonuses[sType] then aBonuses[sType] = math.max(aBonuses[sType], nPerm) end
 			if not aBonuses[sType] then aBonuses[sType] = nPerm end
 		else
+			-- Stack all penalties
 			table.insert(aBonuses, nPerm)
 		end
 	end
