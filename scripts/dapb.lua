edc26a2bc65a8d60b47730333c40d55d32433040
Wil Thieme <wil.thieme@protonmail.com>
2021-08-24 16:25:14 -0400

    add racial ability changes to abilperms

---
+++ scripts/dapb.lua
@@ -0,0 +1,67 @@
+--
+--	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+local handleRacialAbilities_old = nil
+local function handleRacialAbilities_new(nodeChar, sText)
+	local aWords = StringManager.parseWords(sText:lower());
+	
+	local aIncreases = {};
+	local bChoice = false;
+	local i = 1;
+	while aWords[i] do
+		if StringManager.isNumberString(aWords[i]) then
+			local nMod = tonumber(aWords[i]) or 0;
+			if nMod ~= 0 then
+				if StringManager.contains(DataCommon.abilities, aWords[i+1]) then
+					aIncreases[aWords[i+1]] = nMod;
+				elseif StringManager.contains(DataCommon.abilities, aWords[i-1]) then
+					aIncreases[aWords[i-1]] = nMod;
+				else
+					local j = i + 1;
+					if StringManager.isWord(aWords[j], "bonus") then
+						j = j + 1;
+					end
+					if StringManager.isPhrase(aWords, j, { "to", "one", "ability", "score" }) then
+						bChoice = true;
+					end
+				end
+			end
+		end
+		i = i + 1;
+	end
+	
+	local bApplied = false;
+	for k,v in pairs(aIncreases) do
+		if StringManager.contains(DataCommon.abilities, k) then
+			local nodeAbilityPerm = nodeChar.getChild("abilities." .. k).createChild("abilperms").createChild()
+			DB.setValue(nodeAbilityPerm, "name", "string", "Racial Ability Modifier");
+			DB.setValue(nodeAbilityPerm, "bonus_type", "string", "racial");
+			DB.setValue(nodeAbilityPerm, "permnum", "number", v);
+			bApplied = true;
+		end
+	end
+	
+	if bChoice then
+		local aAbilities = {};
+		for _,v in ipairs(DataCommon.abilities) do
+			table.insert(aAbilities, StringManager.capitalize(v));
+		end
+		local wSelect = Interface.openWindow("select_dialog", "");
+		local sTitle = Interface.getString("char_title_selectabilityincrease");
+		local sMessage = Interface.getString("char_message_selectabilityincrease");
+		wSelect.requestSelection(sTitle, sMessage, aAbilities, CharManager.onRaceAbilitySelect, nodeChar, 1);
+		bApplied = true;
+	end
+	
+	return bApplied;
+end
+
+function onInit()
+	handleRacialAbilities_old = CharManager.handleRacialAbilities
+	CharManager.handleRacialAbilities = handleRacialAbilities_new
+end
+
+function onClose()
+	CharManager.handleRacialAbilities = handleRacialAbilities_old
+end
